d3f2baaa6257649353e54563628f5aa4b8f52bb2
Sharizan Mohd Redzuan <ijanzz@Sharizans-MacBook-Pro.local>
2017-10-25 20:41:34 +0800

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+//bootstrap
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule
+    , BsDropdownModule.forRoot()
+    , CarouselModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
